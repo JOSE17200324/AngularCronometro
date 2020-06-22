e39e7795c01d9d48bda46b8e38b280e263a253d2
JACM <jose.condori5@unmsm.edu.pe>
2020-06-22 12:07:17 -0500

    Primer commit JuegoDado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto009';
+   mensaje: string = "";
+  
+   valor1: number = 0;
+   valor2: number = 0;
+   valor3: number = 0;
+
+  constructor () {
+    this.valor1 = this.generarAleatorio();
+    this.valor2 = this.generarAleatorio();
+    this.valor3 = this.generarAleatorio();
+  }
+  public generarAleatorio(): number {
+    return Math.trunc(Math.random() * 6 + 1);
+  }
+  private generarMensaje(): void {
+    if(this.valor1 === this.valor2 && this.valor2 === this.valor3) {
+      this.mensaje = 'GANÓ';
+    } else { this.mensaje = 'PERDIÓ'; }
+  }
+  public tirar(): void {
+    this.valor1 = this.generarAleatorio();
+    this.valor2 = this.generarAleatorio();
+    this.valor3 = this.generarAleatorio();
+    this.generarMensaje();
+  }
 }
